b85c44fa771ecbff71bc7597be1befbd0476f1ea
Karsten Teuber <teuber.karsten@gmail.com>
2017-08-18 14:29:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,59 @@
 <?php
-$random_num = random_int(1, 10);
+session_start();
 
-echo "Die Zufallszahl ist " . $random_num . ".";
-echo "...";
-echo "Deine gew&auml;hlte Zahl ist " . $_POST['zahl'] . ".";
+if (isset($_SESSION['Test'])) {
+    $random_num = $_SESSION['Test'];
+} else {
+    $random_num = random_int(1, 100);
+    $_SESSION['Test'] = $random_num;
+}
+
+//print_r($random_num);
+//echo "Die Zufallszahl ist die " . $random_num . ".";
+//echo "...";
+//echo "Deine gew&auml;hlte Zahl ist die " . $_POST['zahl'] . ".";
+
 $message = "Kann losgehen";
 
-if($_POST['zahl'] == $random_num){
-    echo "Korrekt";
+if ($_POST) {
+
+    if ($_POST['zahl'] == $random_num) {
+        $message = "Korrekt!";
+        unset($_SESSION['Test']);
+        $_SESSION['counter'] ++;
+        $message .= " Du hast " . $_SESSION['counter'] . " Versuche gebraucht.";
+        $_SESSION['counter'] = 0;
+        
+    } else {
+        $message = "Leider Falsch, ";
+        $_SESSION['counter']++;
+
+        if ($_POST['zahl'] < $random_num) {
+            $message = $message . "dein Wert liegt unter der Zufallszahl!";
+        }
+
+        if ($_POST['zahl'] > $random_num) {
+            $message .= "dein Wert liegt &uuml;ber der Zufallszahl";
+        }
+    }
 }
-else{
-    echo "Leider falsch";
-}
+
 
 ?>
 <!DOCTYPE HTML>
 <html>
-<head>
+    <head>
         <meta charset="utf-8">
-	<title>PHP Spielerei</title>
-</head>
-<body>
-    W&auml;hle eine Zahl zwischen 1 und 10!
-    
-    <p><?php echo $message; ?></p>
-    
-    <form method="post">
-        <label>Zahl <input id="auswahl" type="number" name="zahl" min="1" max="10" ></label>
-        <button type="submit" id="übermitteln">Vergleichen</button>
-    </form>
-    
-    <?php
-    
-    ?>
-</body>
+        <title>PHP Spielerei</title>
+    </head>
+    <body>
+        W&auml;hle eine Zahl zwischen 1 und 100!
+        <p><?php echo $message; ?></p>
+        <form method="post">
+            <label>Zahl <input id="auswahl" type="number" name="zahl" min="1" max="100" ></label>
+            <button type="submit" id="übermitteln">Vergleichen</button>
+        </form>
+       
+        
+    </body>
 </html>
